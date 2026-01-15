ea548f6be400298cf1eaf7bcd46deb4bef1d7dc8
unknown <dbsgywls667@gmail.com>
2026-01-15 22:44:01 +0900

    배송안내 페이지 이미지 경로 변경

--- src/features/login/components/SocialLoginButtons.jsx
+++ src/features/login/components/SocialLoginButtons.jsx
@@ -1,10 +1,12 @@
+import { API_BASE_URL } from "shared/constants/apiBaseUrl";
+
 // src/features/auth/components/SocialLoginButtons.jsx
 export function SocialLoginButtons() {
   const handleNaverLogin = () =>
-    (window.location.href = "http://localhost:8080/oauth2/authorization/naver");
+    (window.location.href = `${API_BASE_URL}/oauth2/authorization/naver`);
 
   const handleKakaoLogin = () =>
-    (window.location.href = "http://localhost:8080/oauth2/authorization/kakao");
+    (window.location.href = `${API_BASE_URL}/oauth2/authorization/kakao`);
 
   return (
     <>
