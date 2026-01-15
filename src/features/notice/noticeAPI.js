d95a29d60e30e826e6cac0044e2955a4b75fbcde
unknown <dbsgywls667@gmail.com>
2026-01-15 21:26:42 +0900

    아이디 체크추가\

--- src/features/notice/noticeAPI.js
+++ src/features/notice/noticeAPI.js
@@ -1,8 +1,9 @@
+import { API_BASE_URL } from "shared/constants/apiBaseUrl";
 import { setNoticeList } from "./noticeSlice";
 // shared
 import { api } from "shared/lib/axios";
 
 export const setNoticeListAPI = () => async(dispatch) => {
-    const result = await api.get("http://localhost:8080/notice/all");
+    const result = await api.get(`${API_BASE_URL}/notice/all`);
     dispatch(setNoticeList({"result" : result.data}));
 }
